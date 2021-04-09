7ccb2497266293529f5e8aae53b20ca22eaa8467
Newp <mynewp@gmail.com>
2021-04-10 00:00:02 +0900

    샘플코드 추가

--- DynamoDbExample/DynamoDbExample/Program.cs
+++ DynamoDbExample/DynamoDbExample/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
 
 namespace DynamoDbExample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+            var tableName = "UserResources";
+            var itemResult = await client.GetItemAsync(new GetItemRequest()
+            {
+                TableName = tableName,
+                Key = new System.Collections.Generic.Dictionary<string, AttributeValue>()
+                {
+                    { "UserId", new AttributeValue("test2") },
+                    { "ResourceId", new AttributeValue("c1") },
+                }
+            });
+
+
+            var document = Document.FromAttributeMap(itemResult.Item);
+
+            document["msg"] = "added2";
+            document["UpdatedAt"] = DateTime.Now;
+
+            Console.WriteLine(document.ToJsonPretty());
+
+            var itemResponse = await client.PutItemAsync(new PutItemRequest()
+            {
+                TableName = tableName,
+                Item = document.ToAttributeMap(),
+                //ConditionExpression = 
+            });
+
         }
     }
 }
